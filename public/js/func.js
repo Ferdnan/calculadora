7a740f6a6888adb3eb947bfcde0d4c880178c750
José Fernandes <110367340+Ferdnan@users.noreply.github.com>
2023-03-06 02:31:00 -0300

    feito pequenas correções

--- public/js/func.js
+++ public/js/func.js
@@ -1,34 +1,32 @@
-
-
 function insert(num) {
 
     let numero = document.getElementById('resultado').innerHTML;
-        document.getElementById('resultado').innerHTML = numero + num;
+    document.getElementById('resultado').innerHTML = numero + num;
 }
 
 function clean() {
-    
+
     document.getElementById('resultado').innerHTML = "";
 }
 
 function back() {
 
     let resultado = document.getElementById('resultado').innerHTML;
-        document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
 
 }
 
 function calcular() {
 
     let resultado = document.getElementById('resultado').innerHTML;
-    if(resultado) { 
+    if (resultado) {
 
         document.getElementById('resultado').innerHTML = eval(resultado)
 
     }
-        else {
+    else {
 
-            document.getElementById('resultado').innerHTML = "Nada..."
+        document.getElementById('resultado').innerHTML = "Nada..."
 
-        }
+    }
 }
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-// Imports
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-// Static files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-// Routers 
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/views/index.html')
-})
-
-
-
-
-
-
-// Listen on port 3000
-app.listen(port, () => console.info('Escutando na porta $(port)'))
+app.listen(PORT, () => console.log(
+    `Server is running in ${PORT}`
+));
