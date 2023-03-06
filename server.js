4e47447b3badbda92be1d315f112fd0fe955a6cc
José Fernandes <110367340+Ferdnan@users.noreply.github.com>
2023-03-06 02:34:33 -0300

    teste

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(PORT, () => console.log(
-    `Server is running in ${PORT}`
+app.listen(port, () => console.log(
+    `Server is running in ${port}`
 ));
